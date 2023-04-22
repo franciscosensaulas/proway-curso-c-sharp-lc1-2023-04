99bbe78d32f9c27595b46ef8cba23e474e5a4f49
Francisco Lucas Sens <franciscosensaulas@gmail.com>
2023-04-22 16:49:12 -0300

    Adicionado exemplo vazio do laço de repetição while

---
+++ Fundamentos/LacoRepeticao/LacoRepeticaoWhile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.LacoRepeticao
+{
+    internal class LacoRepeticaoWhile
+    {
+        // Método sem retorno, dica void
+        // Método sem parâmetros, dica n tem variável dentro dos ()
+        public void Executar()
+        {
+            Exemplo01();
+        }
+
+        public void Exemplo01()
+        {
+            Console.WriteLine("OIEEEEE");
+        }
+    }
+}
